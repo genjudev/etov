f445bd378c556f07c413de7d92577014c1cdcd7e
genjudev <lars@feldeisen.de>
2023-12-24 00:20:13 +0100

    Check if the response is an EtovResponse

--- index.ts
+++ index.ts
@@ -1,24 +1,40 @@
+type EtovResponse<T> = [T | null, Error | null];
+
+function isEtovResponse<T>(value: any): value is EtovResponse<T> {
+    return Array.isArray(value) && value.length === 2 &&
+           (value[0] === null || typeof value[0] !== 'undefined') &&
+           (value[1] === null || value[1] instanceof Error);
+}
+
 // Function Overloads
 
 // Overload for asynchronous functions
-function etov<T, Args extends any[]>(func: (...args: Args) => Promise<T>, ...args: Args): Promise<[T | null, Error | null]>;
+function etov<T, Args extends any[]>(func: (...args: Args) => Promise<T>, ...args: Args): Promise<EtovResponse<T>>;
 
 // Overload for synchronous functions
-function etov<T, Args extends any[]>(func: (...args: Args) => T, ...args: Args): [T | null, Error | null];
+function etov<T, Args extends any[]>(func: (...args: Args) => T, ...args: Args): EtovResponse<T>;
 
 // Function Implementation
-function etov<T, Args extends any[]>(func: (...args: Args) => T | Promise<T>, ...args: Args): [T | null, Error | null] | Promise<[T | null, Error | null]> {
+function etov<T, Args extends any[]>(func: (...args: Args) => T | Promise<T>, ...args: Args): EtovResponse<T> | Promise<EtovResponse<T>> {
     try {
         const result = func(...args);
 
         // Check if the function is asynchronous
         if (result instanceof Promise) {
             return result
-                .then((value): [T, null] => [value, null])
-                .catch((error): [null, Error] => [null, error instanceof Error ? error : new Error(String(error))]);
+                .then((value): EtovResponse<T> => {
+                    if (isEtovResponse<T>(value)) {
+                        return value;
+                    }
+                    return [value, null];
+                })
+                .catch((error): EtovResponse<T> => [null, error instanceof Error ? error : new Error(String(error))]);
         }
 
-        // Handle synchronous function
+        if (isEtovResponse<T>(result)) {
+            return result;
+        }
+
         return [result as T, null];
     } catch (error) {
         return [null, error instanceof Error ? error : new Error(String(error))];
